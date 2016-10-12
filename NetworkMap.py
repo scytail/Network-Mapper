bb5036e27d62dff794eba11f5ffdd4be4e0f1e1a
hourglasscode <sourcecodealchemist@gmail.com>
2016-10-12 10:03:55 -0500

    modify h_recur function
    removed exception

--- NetworkMap.py
+++ NetworkMap.py
@@ -21,31 +21,28 @@ def hierarchy_pos(G, root, width=1, vert_gap = 0.2, vert_loc = 0, xcenter = 0.5 
        vert_loc: vertical location of root
        xcenter: horizontal location of root
     '''
-    try:
-        def h_recur(G, root, width=1, vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, 
-                      pos = None, parent = None, parsed = [] ):
-            if(root not in parsed):
-                parsed.append(root)
-                if pos == None:
-                    pos = {root:(xcenter,vert_loc)}
-                else:
-                    pos[root] = (xcenter, vert_loc)
-                neighbors = G.neighbors(root)
-                if parent != None:
-                    neighbors.remove(parent)
-                if len(neighbors)!=0:
-                    dx = width/len(neighbors) 
-                    nextx = xcenter - width/2 - dx/2
-                    for neighbor in neighbors:
-                        nextx += dx
-                        pos = h_recur(G,neighbor, width = dx, vert_gap = vert_gap, 
-                                            vert_loc = vert_loc-vert_gap, xcenter=nextx, pos=pos, 
-                                            parent = root, parsed = parsed)
-            return pos
-            
-        return h_recur(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5)
-    except nx.NetworkXError: 
-        print("NetworkXError")
+    def h_recur(G, root, width=1, vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, 
+                  pos = None, parent = None, parsed = [] ):
+        if(root not in parsed):
+            parsed.append(root)
+            if pos == None:
+                pos = {root:(xcenter,vert_loc)}
+            else:
+                pos[root] = (xcenter, vert_loc)
+            neighbors = G.neighbors(root)
+            if parent != None:
+                neighbors.remove(parent)
+            if len(neighbors)!=0:
+                dx = width/len(neighbors) 
+                nextx = xcenter - width/2 - dx/2
+                for neighbor in neighbors:
+                    nextx += dx
+                    pos = h_recur(G,neighbor, width = dx, vert_gap = vert_gap, 
+                                        vert_loc = vert_loc-vert_gap, xcenter=nextx, pos=pos, 
+                                        parent = root, parsed = parsed)
+        return pos
+        
+    return h_recur(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5)
 
 def ipNetworkFromMask(addressString,subnetMaskString):
     '''
